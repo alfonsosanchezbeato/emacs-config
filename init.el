b0686192c978d07a73300f444dbcf94a47b0c934
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-03-24 15:34:52 +0100

    Disable global-linum-mode
    Just do it in some modes, it can be slightly annoying, for instance
    when showing man pages.

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
 
 (show-paren-mode 1)
 ; emacs 26: see https://emacs.stackexchange.com/questions/278/how-do-i-display-line-numbers-in-emacs-not-in-the-mode-line
-(global-linum-mode 1)
+;(global-linum-mode 1)
 
 ;(require 'uniquify) ; bundled with GNU emacs 23.2.1 or before. On in 24.4
 ; Append directory name to the beginning for buffers with the same name
